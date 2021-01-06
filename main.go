2c39eb833d2dab03a7bfbea3e19b5d02d6943e8b
Georg Hertzsch <Georg.Hertzsch@msg-gillardon.de>
2021-01-06 15:26:05 +0100

    base64

--- main.go
+++ main.go
@@ -1,48 +1,10 @@
 package main
 
 import (
-	"net/http"
+	"encoding/base64"
+	"fmt"
 )
 
-type person struct {
-	First string
-}
-
 func main() {
-	// p1 := person{
-	// 	First: "Jenny",
-	// }
-
-	// p2 := person{
-	// 	First: "James",
-	// }
-
-	// xp := []person{p1, p2}
-
-	// bs, err := json.Marshal(xp)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-	// fmt.Println(string(bs))
-
-	// xp2 := []person{}
-
-	// err = json.Unmarshal(bs, &xp2)
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	// fmt.Println("Back into a Go data structure", xp2)
-
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
-	http.ListenAndServe(":8080", nil)
-}
-
-func foo(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func bar(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
 }
