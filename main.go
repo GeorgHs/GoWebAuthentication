4868532d366f8a5a027e925b016e0139c971f5ff
Georg Hertzsch <Georg.Hertzsch@msg-gillardon.de>
2021-01-06 20:56:18 +0100

    Using standard claims

--- main.go
+++ main.go
@@ -5,9 +5,26 @@ import (
 	"crypto/sha512"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type UserClaims struct {
+	jwt.StandardClaims
+	SessionID int64
+}
+
+func (u *UserClaims) Valid() error {
+	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
+		return fmt.Errorf("Token has expired")
+	}
+	if u.SessionID == 0 {
+		return fmt.Errorf("Invalid session ID")
+	}
+	return nil
+}
 
 var key = []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
 
