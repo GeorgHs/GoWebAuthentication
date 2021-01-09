9fef67ada31209ce3afc677fbef02cb7578cdb0c
Georg Hertzsch <Georg.Hertzsch@msg-gillardon.de>
2021-01-10 00:37:06 +0100

    base64 en- and decoding

--- main.go
+++ main.go
@@ -1,151 +1,32 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha512"
+	"encoding/base64"
 	"fmt"
-	"io"
 	"log"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gofrs/uuid"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type UserClaims struct {
-	jwt.StandardClaims
-	SessionID int64
-}
-
-func (u *UserClaims) Valid() error {
-	if !u.VerifyExpiresAt(time.Now().Unix(), true) {
-		return fmt.Errorf("Token has expired")
-	}
-	if u.SessionID == 0 {
-		return fmt.Errorf("Invalid session ID")
-	}
-	return nil
-}
-
 func main() {
+	msg := "This is totally fun get hands-on and learning it from the ground up."
+	encoded := encode(msg)
+	fmt.Println("ENCODED MSG", encoded)
 
-	pass := "123456789"
-
-	hashedPass, err := hashPassword(pass)
+	s, err := decode(encoded)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
-
-	err = comparePassword(pass, hashedPass)
-	if err != nil {
-		log.Fatalln("Not logged in")
-	}
-	log.Println("Logged in!")
+	fmt.Println("DECODED MSG", s)
 }
 
-func hashPassword(passsword string) ([]byte, error) {
-	bs, err := bcrypt.GenerateFromPassword([]byte(passsword), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, fmt.Errorf("Error while generating bcrypt has from password: %w", err)
-	}
-	return bs, nil
+func encode(msg string) string {
+	encoded := base64.URLEncoding.EncodeToString([]byte(msg))
+	return encoded
 }
 
-func comparePassword(password string, hashedPass []byte) error {
-	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+func decode(encoded string) (string, error) {
+	s, err := base64.URLEncoding.DecodeString(encoded)
 	if err != nil {
-		return fmt.Errorf("Invalid password: %w", err)
+		return "", fmt.Errorf("couldn't decode string %w", err)
 	}
-	return nil
-}
-
-func signMessage(msg []byte) ([]byte, error) {
-	h := hmac.New(sha512.New, keys[currentKid].key)
-	_, err := h.Write(msg)
-	if err != nil {
-		return nil, fmt.Errorf("Error in signMessage while hashing message: %w", err)
-	}
-
-	signed := h.Sum(nil)
-	return signed, nil
-}
-
-func checkSig(msg, sig []byte) (bool, error) {
-	newSig, err := signMessage(msg)
-	if err != nil {
-		return false, fmt.Errorf("Error in checkSig while getting signature of message: %w", err)
-	}
-
-	same := hmac.Equal(newSig, sig)
-	return same, nil
-}
-
-func createToken(c *UserClaims) (string, error) {
-	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-	signedToken, err := t.SignedString(keys[currentKid])
-	if err != nil {
-		return "", fmt.Errorf("Error in createToken when signing Token: %w", err)
-	}
-	return signedToken, nil
-}
-
-type key struct {
-	key     []byte
-	created time.Time
-}
-
-var currentKid = ""
-var keys = map[string]key{}
-
-func parseToken(signedToken string) (*UserClaims, error) {
-	t, err := jwt.ParseWithClaims(signedToken, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
-		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
-			return nil, fmt.Errorf("Invalid signing algorithm")
-		}
-
-		kid, ok := t.Header["kid"].(string)
-		if !ok {
-			return nil, fmt.Errorf("Invalid key ID")
-		}
-
-		k, ok := keys[kid]
-		if !ok {
-			return nil, fmt.Errorf("Invalid key ID")
-		}
-
-		return k.key, nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("Error in parseToken while parsing token: %w", err)
-	}
-
-	if !t.Valid {
-		return nil, fmt.Errorf("Error in parseToken, token is not valid")
-	}
-
-	return t.Claims.(*UserClaims), nil
-}
-
-func generateNewKey() error {
-	newKey := make([]byte, 64)
-	_, err := io.ReadFull(rand.Reader, newKey)
-	if err != nil {
-		return fmt.Errorf("Error in generateNewKey while generating key: %w", err)
-	}
-
-	uid, err := uuid.NewV4()
-	if err != nil {
-		return fmt.Errorf("Error in generateNewKey whil generating kid: %w", err)
-	}
-
-	keys[uid.String()] = key{
-		key:     newKey,
-		created: time.Now(),
-	}
-	currentKid = uid.String()
-
-	return nil
-
+	return string(s), nil
 }
